735fcb91dc934b2db40e645b8b94d1c2ff68062b
Olajutemo Timilehin <olajutemot@gmail.com>
2023-10-30 02:00:09 -0700

    first commit

---
+++ script.js
@@ -0,0 +1,54 @@
+// JavaScript for opening and closing the portfolio overlay
+const openPortfolioButton = document.getElementById("openPortfolio");
+const closePortfolioButton = document.getElementById("closePortfolio");
+const portfolioOverlay = document.getElementById("portfolioOverlay");
+const projectButton = document.getElementById("projects-button");
+const skillButton = document.getElementById("skills-button");
+const skill = document.getElementById("skills");
+const project = document.getElementById("projects");
+const darkButton = document.querySelector(".fa-moon");
+const lightButton = document.querySelector(".fa-sun");
+
+// ==============================setting dark and light theme====================================
+darkButton.addEventListener("click", () => {
+  lightButton.style.display = "block";
+  darkButton.style.display = "none";
+  portfolioOverlay.classList.remove("lightmode");
+  portfolioOverlay.classList.add("darkmode");
+});
+lightButton.addEventListener("click", () => {
+  lightButton.style.display = "none";
+  darkButton.style.display = "block";
+  portfolioOverlay.classList.add("lightmode");
+  portfolioOverlay.classList.remove("darkmode");
+});
+
+//===========================opening and closinf portfolio===================================
+openPortfolioButton.addEventListener("click", () => {
+  portfolioOverlay.style.display = "flex";
+});
+
+closePortfolioButton.addEventListener("click", () => {
+  portfolioOverlay.style.display = "none";
+});
+// ==========================switching project and skills=================================
+skillButton.addEventListener("click", () => {
+  if (projectButton.classList.contains("project-toggle-active")) {
+    projectButton.classList.remove("project-toggle-active");
+    skillButton.classList.add("project-toggle-active");
+    skill.style.display = "contents";
+    project.style.display = "none";
+  } else {
+    skillButton.classList.add("project-toggle-active");
+  }
+});
+projectButton.addEventListener("click", () => {
+  if (projectButton.classList.contains("project-toggle-active")) {
+    projectButton.classList.add("project-toggle-active");
+  } else {
+    skillButton.classList.remove("project-toggle-active");
+    projectButton.classList.add("project-toggle-active");
+    skill.style.display = "none";
+    project.style.display = "contents";
+  }
+});
